e477ea416d75f3dcf2defa87783f0eac70e27db5
Sai Shanmukha Narumanchi <nss10@outlook.com>
2020-05-29 23:51:40 -0500

    Added Australia Data

---
+++ code/prepare_data_AUS.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import urllib.request
+import json
+
+def get_AUS_Confirmed_DF(filename):
+    df = pd.read_csv(filename, delimiter=',')
+    df = df[df['Country/Region']=='Australia']
+    df = df.drop(columns=['Country/Region', 'Lat', 'Long'])
+    columns = df.columns.tolist()
+    dates = columns[1:]
+    start = dates[0]
+    end = dates[-1]
+    date_range = pd.date_range(start=start, end=end)
+    df.columns = ['Province_State'] + [i for i in range(0,len(date_range))]
+    return [df, start, end, dates]
+
+def get_US_Death_DF(filename):
+    filename='../data/raw/global/time_series_covid19_confirmed_global.csv'
+    df = pd.read_csv(filename, delimiter=',')
+    df = df[df['Country/Region']=='Australia']
+    df = df.drop(columns=['Country/Region', 'Lat', 'Long'])
+    columns = df.columns.tolist()
+    dates = columns[1:]
+    start = dates[0]
+    end = dates[-1]
+    date_range = pd.date_range(start=start, end=end)
+    df.columns = ['Province_State'] + [i for i in range(0,len(date_range))]
+    return [df, start, end, dates]
+
+def update():
+    url_confirmed = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+    filename_confirmed = '../data/raw/global/time_series_covid19_confirmed_global.csv'
+    url_deaths = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv'
+    filename_deaths = '../data/raw/global/time_series_covid19_deaths_global.csv'
+
+    print(f'Downloading {url_confirmed}')
+    filename_confirmed, headers_confirmed = urllib.request.urlretrieve(url_confirmed, filename=filename_confirmed)
+    print("download complete!")
+    print("download file location: ", filename_confirmed)
+    print("Processing")
+
+    print(f'Downloading {url_deaths}')
+    filename_deaths, headers_deaths = urllib.request.urlretrieve(url_deaths, filename=filename_deaths)
+    print("download complete!")
+    print("download file location: ", filename_deaths)
+    print("Processing")
+
+def prepare():
+    filename_confirmed = '../data/raw/global/time_series_covid19_confirmed_global.csv'
+    filename_deaths = '../data/raw/global/time_series_covid19_deaths_global.csv'
+
+    [df, start, end, dates] = get_AUS_Confirmed_DF(filename_confirmed)
+    df.to_csv('../data/processed/aus/confirmed.csv', index=False)
+    with open('../data/processed/aus/confirmed.json', 'w', encoding='utf-8') as f:
+        json.dump({'start':start, 'end': end, 'dates': dates}, f, ensure_ascii=False, indent=4)
+
+    [df, start, end, dates] = get_US_Death_DF(filename_deaths)
+    df.to_csv('../data/processed/aus/deaths.csv', index=False)
+    with open('../data/processed/aus/deaths.json', 'w', encoding='utf-8') as f:
+        json.dump({'start':start, 'end': end, 'dates': dates}, f, ensure_ascii=False, indent=4)
+
---
+++ code/run_AUS.py
@@ -0,0 +1,36 @@
+from prepare_data_IN import update, prepare
+from mrsc_score import score_rsc, score_mrsc
+import json
+
+
+lockdown_state = [
+    {'name':'Australian Capital Territory','start': '3/13/20', 'end':''},
+    {'name':'New South Wales','start': '3/13/20', 'end':''},
+    {'name':'Northern Territory', 'start': '3/13/20', 'end':''},
+    {'name':'Queensland', 'start': '3/13/20', 'end':''},
+    {'name':'South Australia', 'start': '3/13/20', 'end':''},
+    {'name':'Tasmania', 'start': '3/13/20', 'end':''},
+    {'name':'Victoria', 'start': '3/13/20', 'end':''},
+    {'name':'Western Australia', 'start': '3/13/20', 'end':''}  
+]
+
+def main():
+    update()
+    prepare()
+    metric = ['deaths', 'confirmed']
+    file2 = f'../data/processed/aus/{metric[1]}.csv'
+    file1 = f'../data/processed/aus/{metric[0]}.csv'
+
+    metadata = None
+    with open(f'../data/processed/aus/{metric[0]}.json') as f:
+        metadata = json.load(f)
+
+    for state in lockdown_state:
+        name = state['name']
+        lockdown_date = state['start']
+        lockdown_date_index = metadata['dates'].index(lockdown_date)
+        end_date_index = len(metadata['dates'])
+        score_mrsc(file1, file2,'Province_State', name, 0, lockdown_date_index, end_date_index, metric[0], 'aus')
+
+if __name__ == "__main__":
+    main()  
